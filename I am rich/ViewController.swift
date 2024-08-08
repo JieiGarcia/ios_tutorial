e84d5231ebd208c4faf858e27404551418f5fb6b
JieiGarcia <jiei.aca@gmail.com>
2024-08-08 23:04:16 +0900

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Jay Garcia on 2024/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
